5e147fcf912fb78e2ae4128c5e9c478b12820dc0
Brian Cristante <brcrista@microsoft.com>
2019-12-12 13:40:08 -0500

    Get bound ports in C++

--- iphlpapi_test/iphlpapi_test.cpp
+++ iphlpapi_test/iphlpapi_test.cpp
@@ -1,33 +1,41 @@
 #include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <string>
 
 #include <Windows.h>
 #include <iphlpapi.h>
 
+// Exported from iphlpapi.dll, but is not in iphlpapi.h
+extern "C" __declspec(dllimport) DWORD __stdcall InternalGetBoundTcpEndpointTable(
+    PMIB_TCPTABLE2* ppTcpTable,
+    HANDLE hHeap,
+    DWORD dwFlags);
+
 using namespace std;
 
 string to_tcp_state(DWORD connection_state)
 {
+    // See tcpmib.h
     switch (connection_state)
     {
-    case 1: return "CLOSED";
-    case 2: return "LISTEN";
-    case 3: return "SYN-SENT";
-    case 4: return "SYN-RECEIVED";
-    case 5: return "ESTABLISHED";
-    case 6: return "FIN-WAIT-1";
-    case 7: return "FIN-WAIT-2";
-    case 8: return "CLOSE-WAIT";
-    case 9: return "CLOSING";
-    case 10: return "LAST-ACK";
-    case 11: return "TIME-WAIT";
-    case 12: return "DELETE TCB";
+    case MIB_TCP_STATE_CLOSED: return "CLOSED";
+    case MIB_TCP_STATE_LISTEN: return "LISTEN";
+    case MIB_TCP_STATE_SYN_SENT: return "SYN-SENT";
+    case MIB_TCP_STATE_SYN_RCVD: return "SYN-RECEIVED";
+    case MIB_TCP_STATE_ESTAB: return "ESTABLISHED";
+    case MIB_TCP_STATE_FIN_WAIT1: return "FIN-WAIT-1";
+    case MIB_TCP_STATE_FIN_WAIT2: return "FIN-WAIT-2";
+    case MIB_TCP_STATE_CLOSE_WAIT: return "CLOSE-WAIT";
+    case MIB_TCP_STATE_CLOSING: return "CLOSING";
+    case MIB_TCP_STATE_LAST_ACK: return "LAST-ACK";
+    case MIB_TCP_STATE_TIME_WAIT: return "TIME-WAIT";
+    case MIB_TCP_STATE_DELETE_TCB: return "DELETE TCB";
     default: return "UNKNOWN (" + to_string(connection_state) + ")";
     }
 }
 
-void print_tcp_connection(const MIB_TCPROW2& connection)
+void print_tcp_connection(const MIB_TCPROW2& connection, const function<string(DWORD)>& get_tcp_state)
 {
     cout
         << "Local address: "
@@ -35,7 +43,7 @@ void print_tcp_connection(const MIB_TCPROW2& connection)
         << "Remote address: "
         << connection.dwRemoteAddr << ":" << connection.dwRemotePort << " "
         << "State: "
-        << to_tcp_state(connection.dwState)
+        << get_tcp_state(connection.dwState)
         << endl;
 }
 
@@ -61,35 +69,66 @@ int main()
     switch (status)
     {
     case NO_ERROR:
+        // Print the table of connnections
         tcp_table_ipv4 = reinterpret_cast<PMIB_TCPTABLE2>(buffer);
         cout << "Found " << tcp_table_ipv4->dwNumEntries << " connections:" << endl;
         for (DWORD i = 0; i < tcp_table_ipv4->dwNumEntries; i++)
         {
             const auto row = tcp_table_ipv4->table[i];
-            print_tcp_connection(row);
+            print_tcp_connection(row, to_tcp_state);
         }
-
-        free(buffer);
-        return 0;
+        break;
     case ERROR_INSUFFICIENT_BUFFER:
         cerr << "Insufficient buffer: required " << buffer_size << " bytes." << endl;
-
-        free(buffer);
-        return 1;
+        break;
     case ERROR_INVALID_PARAMETER:
         cerr << "Could not write to the buffer. Is it null?" << endl;
-
-        free(buffer);
-        return 1;
+        break;
     case ERROR_NOT_SUPPORTED:
         cerr << "Not supported on this OS." << endl;
-
-        free(buffer);
-        return 1;
+        break;
     default:
         cerr << "An unknown error occurred." << endl;
+        break;
+    }
 
-        free(buffer);
+    free(buffer);
+    if (status != NO_ERROR)
+    {
         return 1;
     }
+
+
+    PMIB_TCPTABLE2 tcp_table_bound;
+    HANDLE heap = ::GetProcessHeap();
+
+    status = ::InternalGetBoundTcpEndpointTable(
+        &tcp_table_bound,
+        heap,
+        0);
+
+    // Print the table of bound ports
+    if (status == NO_ERROR)
+    {
+        cout << endl;
+
+        cout << "Found " << tcp_table_bound->dwNumEntries << " bound ports:" << endl;
+        for (DWORD i = 0; i < tcp_table_bound->dwNumEntries; i++)
+        {
+            const auto row = tcp_table_bound->table[i];
+            print_tcp_connection(row, [] (DWORD _) { return "BOUND"; });
+        }
+    }
+    else
+    {
+        cerr << "An unknown error occurred." << endl;
+    }
+
+    ::HeapFree(heap, 0, tcp_table_bound);
+    if (status != NO_ERROR)
+    {
+        return 1;
+    }
+
+    return 0;
 }
