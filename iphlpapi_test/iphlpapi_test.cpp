e065492730463a475abf9d8e292609b5cdbef170
Brian Cristante <brcrista@microsoft.com>
2019-12-11 20:05:28 -0500

    C++: Print TCP states as strings

--- iphlpapi_test/iphlpapi_test.cpp
+++ iphlpapi_test/iphlpapi_test.cpp
@@ -1,10 +1,31 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 #include <Windows.h>
 #include <iphlpapi.h>
 
 using namespace std;
+
+string to_tcp_state(DWORD connection_state)
+{
+    switch (connection_state)
+    {
+    case 1: return "CLOSED";
+    case 2: return "LISTEN";
+    case 3: return "SYN-SENT";
+    case 4: return "SYN-RECEIVED";
+    case 5: return "ESTABLISHED";
+    case 6: return "FIN-WAIT-1";
+    case 7: return "FIN-WAIT-2";
+    case 8: return "CLOSE-WAIT";
+    case 9: return "CLOSING";
+    case 10: return "LAST-ACK";
+    case 11: return "TIME-WAIT";
+    case 12: return "DELETE TCB";
+    default: return "UNKNOWN (" + to_string(connection_state) + ")";
+    }
+}
 
 void print_tcp_connection(const MIB_TCPROW2& connection)
 {
@@ -14,40 +35,28 @@ void print_tcp_connection(const MIB_TCPROW2& connection)
         << "Remote address: "
         << connection.dwRemoteAddr << ":" << connection.dwRemotePort << " "
         << "State: "
-        << connection.dwState
+        << to_tcp_state(connection.dwState)
         << endl;
 }
 
 int main()
 {
     ULONG status;
-    ULONG buffer_size = sizeof(MIB_TCPTABLE2);
-    void* buffer = malloc(sizeof(MIB_TCPTABLE2));
+    ULONG buffer_size = 0;
 
     // Call for size
     status = ::GetTcpTable2(
-        reinterpret_cast<PMIB_TCPTABLE2>(buffer),
+        nullptr,
         &buffer_size,
-        TRUE);
+        true);
 
-    if (status == ERROR_INSUFFICIENT_BUFFER)
-    {
-        free(buffer);
-        buffer = malloc(buffer_size);
-    }
-    else if (status != NO_ERROR)
-    {
-        cerr << "Something is wrong, we didn't get the size back. Status code: " << status << endl;
-        free(buffer);
-        return 1;
-    }
-
+    void* buffer = malloc(buffer_size);
     status = ::GetTcpTable2(
         reinterpret_cast<PMIB_TCPTABLE2>(buffer),
         &buffer_size,
-        TRUE);
+        true);
 
-    PMIB_TCPTABLE2 tcp_table_ipv4 = NULL;
+    PMIB_TCPTABLE2 tcp_table_ipv4 = nullptr;
 
     switch (status)
     {
